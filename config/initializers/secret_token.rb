f12a48746fcf31b681d319b59206967e632cb620
Jay Moorthi <jay@tddium.com>
2011-09-21 20:37:51 -0700

    rails app shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlSample::Application.config.secret_token = '9e7ccadf13d2c2f51936deb4205a4bde547844adc3007c06834579575555afaf2d5e9a8c990b21b9fc386a19673e1d947eb51e6f56662f2de64c2c630445a9ad'
